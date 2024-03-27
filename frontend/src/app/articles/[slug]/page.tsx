ee853f770c646e88af71c3077b3dc877af2e7555
jijinbei <hikoharu14142@outlook.jp>
2024-03-27 18:03:29 +0900

    https://zenn.dev/azukiazusa/articles/next-js-app-dir-tutorial のコピペ

---
+++ frontend/src/app/articles/[slug]/page.tsx
@@ -0,0 +1,64 @@
+import { notFound } from "next/navigation";
+import { Article, Comment } from "../../types";
+import { Suspense } from "react";
+import ArticleContent from "./ArticleContent";
+import Comments from "./Comments";
+import { Heading } from "../../common/components";
+import LoadingComments from "./LoadingComments";
+
+const getArticle = async (slug: string) => {
+  const res = await fetch(`http://localhost:3000/api/articles/${slug}`, {
+    next: { revalidate: 60 },
+  });
+
+  if (res.status === 404) {
+    // notFound 関数を呼び出すと not-fount.tsx を表示する
+    notFound();
+  }
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch article");
+  }
+
+  const data = await res.json();
+  return data as Article;
+};
+
+const getComments = async (slug: string) => {
+  const res = await fetch(
+    `http://localhost:3000/api/articles/${slug}/comments`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch comments");
+  }
+
+  const data = await res.json();
+  return data as Comment[];
+};
+
+export default async function ArticleDetail({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const articlePromise = getArticle(params.slug);
+  const commentPromise = getComments(params.slug);
+
+  const article = await articlePromise;
+
+  return (
+    <div>
+      <ArticleContent article={article} />
+      <Heading as="h2" mt={8} mb={4}>
+        Comments
+      </Heading>
+      <Suspense fallback={<LoadingComments />}>
+        <Comments commentPromise={commentPromise} />
+      </Suspense>
+    </div>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,32 @@
+import type { Article } from "./types";
+import ArticleList from "./ArticleList";
+import { Heading } from "./common/components";
+
+async function getArticles() {
+  const res = await fetch("http://localhost:3000/api/articles", {
+    cache: "no-store",
+  });
+
+  // throw new Error("Failed to fetch articles");
+
+  // エラーハンドリングを行うことが推奨されている
+  if (!res.ok) {
+    throw new Error("Failed to fetch articles");
+  }
+
+  const data = await res.json();
+  return data.articles as Article[];
+}
+
+export default async function Home() {
+  const articles = await getArticles();
+
+  return (
+    <div>
+      <Heading as="h1" mb={4}>
+        新着記事
+      </Heading>
+      <ArticleList articles={articles} />
+    </div>
+  );
+}
