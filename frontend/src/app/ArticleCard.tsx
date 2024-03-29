785228687387ab1bbbd55c99a55e0060739e38ca
mikayu117 <128677385+mikayu117@users.noreply.github.com>
2024-03-29 17:16:12 +0900

    install latex

--- frontend/src/app/ArticleCard.tsx
+++ frontend/src/app/ArticleCard.tsx
@@ -8,6 +8,12 @@ import {
 } from "./common/components";
 import NextLink from "next/link";
 import { Article } from "./types";
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import './articles/[slug]/markdown.css';
+import 'katex/dist/katex.min.css';
 
 export default function ArticleCard({ article }: { article: Article }) {
     const formattedDate = new Date(article.createdAt).toLocaleDateString(
@@ -31,7 +37,9 @@ export default function ArticleCard({ article }: { article: Article }) {
                     <Heading size="md">{article.title}</Heading>
                 </CardHeader>
                 <CardBody>
-                    <Text>{article.content.substring(0, 200)}...</Text>
+                    <ReactMarkdown className={"markdown"} remarkPlugins={[remarkMath, remarkGfm]} rehypePlugins={[rehypeKatex]}>
+                    {article.content.substring(0,200)}
+                    </ReactMarkdown>
                 </CardBody>
                 <CardFooter>
                     <Text fontSize="sm" color="gray.600">
--- frontend/src/app/articles/[slug]/ArticleContent.tsx
+++ frontend/src/app/articles/[slug]/ArticleContent.tsx
@@ -1,39 +1,32 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    Text,
-    Heading,
-  } from "../../common/components";
-  import { Article } from "../../types";
-  // import { BlockMath } from "react-katex";
-  import ReactMarkdown from 'react-markdown';
-  // import remarkGfm from 'remark-gfm';
-  import remarkMath from 'remark-math';
-  import rehypeKatex from 'rehype-katex';
+  Card,
+  CardHeader,
+  CardBody,
+  Text,
+  Heading,
+} from "../../common/components";
+import { Article } from "../../types";
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import './markdown.css';
+import 'katex/dist/katex.min.css';
 
-  export default function ArticleContent({ article }: { article: Article }) {
-    const markdownString = `
-# Markdown
+export default function ArticleContent({ article }: { article: Article }) {
 
-Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, nemo!
-`;
-    return (
-      <ReactMarkdown>
-          {markdownString}
-        </ReactMarkdown>
-      // <Card as="article">
-      //   <CardHeader>
-      //     <Heading as="h1">{article.title}</Heading>
-      //   </CardHeader>
-      //   <CardBody>
-      //   <ReactMarkdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
-      //     {article.content}
-      //   </ReactMarkdown>
-      //     <Text as="p" fontSize="md">
-      //       {article.content}
-      //     </Text>
-      //   </CardBody>
-      // </Card>
-    );
-  }
+  return (
+    <Card as="article">
+      <CardHeader>
+        <Heading as="h1">{article.title}</Heading>
+      </CardHeader>
+      <CardBody>
+        <div>
+          <ReactMarkdown className={"markdown"} remarkPlugins={[remarkMath, remarkGfm]} rehypePlugins={[rehypeKatex]}>
+            {article.content}
+          </ReactMarkdown>
+        </div>
+      </CardBody>
+    </Card>
+  );
+}
